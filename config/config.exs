c95a447117b7c6be804dba1e4d778b5e71320dd6
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2019-08-08 01:49:41 -0700

    Remove test file

--- config/config.exs
+++ config/config.exs
@@ -1,6 +1,3 @@
 import Config
 
-config :okta_api, :tesla, adapter: {Tesla.Adapter.Hackney, [recv_timeout: 30_000]}
-
-env_config = "#{Mix.env()}.exs"
-File.exists?("config/#{env_config}") && import_config(env_config)
+config :okta_api, :tesla, adapter: Okta.Tesla.Mock
--- config/test.exs
+++
@@ -1,3 +0,0 @@
-import Config
-
-config :okta_api, :tesla, adapter: Okta.Tesla.Mock
