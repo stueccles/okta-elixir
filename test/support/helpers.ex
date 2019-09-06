ee8ab3e30c57b29b488144674051f0afb565e55b
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2019-09-05 23:59:34 -0700

    Add test support helpers

---
+++ test/support/helpers.ex
@@ -0,0 +1,28 @@
+defmodule Okta.TestSupport.Helpers do
+  @moduledoc false
+
+  import ExUnit.Assertions
+
+  def mock_request(base_url, opts, callback \\ nil) do
+    status = Keyword.get(opts, :status)
+    path = Keyword.get(opts, :path)
+    method = Keyword.get(opts, :method)
+    query = Keyword.get(opts, :query, [])
+    response = Keyword.get(opts, :response, %{})
+    body = Keyword.get(opts, :body)
+
+    Mox.expect(Okta.Tesla.Mock, :call, fn
+      request, _opts ->
+        assert(request.body == body)
+        assert(request.method == method)
+        assert(request.query == query)
+        assert(request.url == base_url <> path)
+
+        if callback do
+          callback.()
+        end
+
+        {:ok, Tesla.Mock.json(response, status: status)}
+    end)
+  end
+end
