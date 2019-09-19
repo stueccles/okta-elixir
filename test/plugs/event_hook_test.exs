92503dc0b492b7269add343da1ee9c02602e43d0
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2019-09-19 15:31:00 -0700

    Testing receiving events

--- test/plugs/event_hook_test.exs
+++ test/plugs/event_hook_test.exs
@@ -1,7 +1,7 @@
 defmodule Okta.Plug.EventHookTest do
   use ExUnit.Case, async: true
-  use Plug.Test
 
+  alias Plug.Test
   alias Plug.Conn
   alias Okta.Plug.EventHook
 
@@ -13,7 +13,7 @@ defmodule Okta.Plug.EventHookTest do
   test "returns 404 with unauthorized requests" do
     conn =
       :post
-      |> conn("/okta/event-hooks")
+      |> Plug.Test.conn("/okta/event-hooks")
       |> EventHook.call(@opts)
 
     assert conn.status == 404
@@ -22,12 +22,27 @@ defmodule Okta.Plug.EventHookTest do
   test "veryfying event hooks" do
     conn =
       :get
-      |> conn("/okta/event-hooks")
+      |> Plug.Test.conn("/okta/event-hooks")
       |> Conn.put_req_header("x-okta-verification-challenge", "my verification challenge")
       |> Conn.put_req_header("authorization", "authorization token")
       |> EventHook.call(@opts)
 
     assert conn.status == 200
     assert conn.resp_body == Jason.encode!(%{verification: "my verification challenge"})
+  end
+
+  test "receiving events" do
+    Mox.expect(EventHookHandlerMock, :handle_event, fn params ->
+      assert params == "some data"
+    end)
+
+    conn =
+      :post
+      |> Plug.Test.conn("/okta/event-hooks")
+      |> Conn.put_req_header("authorization", "authorization token")
+      |> Map.put(:body_params, "some data")
+      |> EventHook.call(@opts)
+
+    assert conn.status == 204
   end
 end
