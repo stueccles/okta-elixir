978ff09bdbdd315a129a722ef158e72a73603178
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2019-09-19 17:18:55 -0700

    Add docs

--- lib/okta/event_hook_handler.ex
+++ lib/okta/event_hook_handler.ex
@@ -1,4 +1,12 @@
 defmodule Okta.EventHookHandler do
+  @moduledoc """
+  Event Hook Handler handles incoming Event Hook from Okta.
+
+  Read more about Event Hooks in the official documation:
+
+    * https://developer.okta.com/docs/concepts/event-hooks/
+  """
+
   @type event :: %{}
 
   @callback handle_event(event :: event()) :: none()
